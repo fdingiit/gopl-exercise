1585cb32aad70fdd217891edf658ab0bbde7ddac
Fei Ding <fdingiit@gmail.com>
2018-03-01 23:01:02 +0800

    1.4

---
+++ 1.4/dup2.go
@@ -0,0 +1,58 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 10.
+//!+
+
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			if countLines(f, counts) {
+				fmt.Printf("%s\n", arg)
+			}
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) bool {
+	hasDup := false
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		counts[line]++
+
+		if !hasDup && counts[line] > 1 {
+			hasDup = true
+		}
+	}
+
+	return hasDup
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+//!-
